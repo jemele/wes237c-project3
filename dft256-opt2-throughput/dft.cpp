8f9ad93283819b883e6d2e324d7512d2305e9d4b
Joshua Emele <jemele@acm.org>
2015-11-15 13:05:13 -0800

    throughput: completely partition, and clean up

---
+++ dft256-opt2-throughput/dft.cpp
@@ -0,0 +1,31 @@
+#include "dft.h"
+#include "coefficients256.h"
+
+void dft_inner(int k, DTYPE real_sample[SIZE], DTYPE imag_sample[SIZE], DTYPE &outreal, DTYPE &outimag)
+{
+#pragma HLS ARRAY_PARTITION variable=sin_coefficients_table complete dim=1
+#pragma HLS ARRAY_PARTITION variable=cos_coefficients_table complete dim=1
+#pragma HLS unroll
+	outreal = 0;
+	outimag = 0;
+	unsigned char angle = 0;
+	for (int t = 0; t < SIZE; ++t, angle += k) {
+		const DTYPE cos_angle = cos_coefficients_table[angle];
+		const DTYPE sin_angle = -sin_coefficients_table[angle];
+		outreal +=  real_sample[t] * cos_angle + imag_sample[t] * sin_angle;
+		outimag += -real_sample[t] * sin_angle + imag_sample[t] * cos_angle;
+	}
+}
+
+void dft(DTYPE real_sample[SIZE], DTYPE imag_sample[SIZE], DTYPE outreal[SIZE], DTYPE outimag[SIZE])
+{
+#pragma HLS ARRAY_PARTITION variable=outreal complete dim=1
+#pragma HLS ARRAY_PARTITION variable=outimag complete dim=1
+#pragma HLS ARRAY_PARTITION variable=imag_sample complete dim=1
+#pragma HLS ARRAY_PARTITION variable=real_sample complete dim=1
+
+    for (int k = 0; k < SIZE; ++k) {
+#pragma HLS pipeline
+    	dft_inner(k,real_sample,imag_sample,outreal[k],outimag[k]);
+    }
+}
