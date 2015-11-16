e402b86478ae4867e6b6af1ccf71c0f93c735979
Joshua Emele <jemele@acm.org>
2015-11-15 19:13:48 -0800

    dft256 fixed size: increase DTYPE bitwidth
    This results in outreal within .005 of out.gold.dat.

--- dft256-opt4-size-fixed/dft.h
+++ dft256-opt4-size-fixed/dft.h
@@ -1,5 +1,5 @@
 #include <ap_fixed.h>
-typedef ap_fixed<32,15> DTYPE;
+typedef ap_fixed<35,15> DTYPE;
 #define SIZE 256
 void dft(DTYPE real_sample[SIZE], DTYPE imag_sample[SIZE], DTYPE outreal[SIZE], DTYPE outimag[SIZE]);
 
