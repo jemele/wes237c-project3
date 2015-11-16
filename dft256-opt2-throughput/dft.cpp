14b0aa2419bd835b093953f24b3bd978b2e3f27c
Joshua Emele <jemele@acm.org>
2015-11-15 21:06:06 -0800

    The final throughput?

--- dft256-opt2-throughput/dft.cpp
+++ dft256-opt2-throughput/dft.cpp
@@ -4,13 +4,12 @@
 
 void dft_inner(int k, DTYPE real_sample[SIZE], DTYPE imag_sample[SIZE], DTYPE &outreal, DTYPE &outimag)
 {
-#pragma HLS ARRAY_PARTITION variable=sin_coefficients_table complete dim=1
-#pragma HLS ARRAY_PARTITION variable=cos_coefficients_table complete dim=1
 #pragma HLS unroll
 	outreal = 0;
 	outimag = 0;
 	ap_uint<8> angle = 0;
 	for (int t = 0; t < SIZE; ++t, angle += k) {
+#pragma HLS unroll
 		const DTYPE cos_angle = cos_coefficients_table[angle];
 		const DTYPE sin_angle = -sin_coefficients_table[angle];
 		outreal +=  real_sample[t] * cos_angle + imag_sample[t] * sin_angle;
@@ -20,11 +19,7 @@ void dft_inner(int k, DTYPE real_sample[SIZE], DTYPE imag_sample[SIZE], DTYPE &o
 
 void dft(DTYPE real_sample[SIZE], DTYPE imag_sample[SIZE], DTYPE outreal[SIZE], DTYPE outimag[SIZE])
 {
-#pragma HLS ARRAY_PARTITION variable=outreal complete dim=1
-#pragma HLS ARRAY_PARTITION variable=outimag complete dim=1
-#pragma HLS ARRAY_PARTITION variable=imag_sample complete dim=1
-#pragma HLS ARRAY_PARTITION variable=real_sample complete dim=1
-
+#pragma HLS dataflow
     for (int k = 0; k < SIZE; ++k) {
 #pragma HLS pipeline
     	dft_inner(k,real_sample,imag_sample,outreal[k],outimag[k]);
