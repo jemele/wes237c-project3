455514c7e84bf404c704c8ae878742a818305b98
Joshua Emele <jemele@acm.org>
2015-11-15 18:45:38 -0800

    use hls::stream

--- dft256-opt5-stream/dft.cpp
+++ dft256-opt5-stream/dft.cpp
@@ -1,21 +1,24 @@
 #include "dft.h"
 #include "coefficients256.h"
 #include <ap_int.h>
+#include <hls_stream.h>
 
-void dft_inner(int k, DTYPE real_sample[SIZE], DTYPE imag_sample[SIZE], DTYPE &outreal, DTYPE &outimag)
+static void dft_inner(int k, DTYPE real_sample[SIZE], DTYPE imag_sample[SIZE], hls::stream<DTYPE> &outreal, hls::stream<DTYPE> &outimag)
 {
-	outreal = 0;
-	outimag = 0;
+	DTYPE sumreal = 0;
+	DTYPE sumimag = 0;
     ap_uint<8> angle = 0;
 	for (int t = 0; t < SIZE; ++t, angle += k) {
 		const DTYPE cos_angle = cos_coefficients_table[angle];
 		const DTYPE sin_angle = -sin_coefficients_table[angle];
-		outreal +=  real_sample[t] * cos_angle + imag_sample[t] * sin_angle;
-		outimag += -real_sample[t] * sin_angle + imag_sample[t] * cos_angle;
+		sumreal +=  real_sample[t] * cos_angle + imag_sample[t] * sin_angle;
+		sumimag += -real_sample[t] * sin_angle + imag_sample[t] * cos_angle;
 	}
+    outreal.write(sumreal);
+    outreal.write(sumimag);
 }
 
-void dft(DTYPE real_sample[SIZE], DTYPE imag_sample[SIZE], DTYPE outreal[SIZE], DTYPE outimag[SIZE])
+void dft(DTYPE real_sample[SIZE], DTYPE imag_sample[SIZE], hls::stream<DTYPE> &outreal, hls::stream<DTYPE> &outimag)
 {
     for (int k = 0; k < SIZE; ++k) {
     	dft_inner(k,real_sample,imag_sample,outreal[k],outimag[k]);
