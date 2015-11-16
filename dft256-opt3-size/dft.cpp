9aa3d38d681d894b0dfb68a325c42bdc17746491
Joshua Emele <jemele@acm.org>
2015-11-15 19:35:10 -0800

    prefer explicit local storage

--- dft256-opt3-size/dft.cpp
+++ dft256-opt3-size/dft.cpp
@@ -19,18 +19,19 @@ static DTYPE sub(DTYPE a, DTYPE b)
 
 static void dft_inner(int k, DTYPE real_sample[SIZE], DTYPE imag_sample[SIZE], DTYPE &outreal, DTYPE &outimag)
 {
-    outreal = 0;
-    outimag = 0;
+    DTYPE sumreal = 0;
+    DTYPE sumimag = 0;
     ap_uint<8>angle = 0;
     for (int t = 0; t < SIZE; ++t, angle += k) {
         const DTYPE cos_angle = cos_coefficients_table[angle];
         const DTYPE sin_angle = sin_coefficients_table[angle];
-
         const DTYPE real_part = sub(mult(real_sample[t],cos_angle),mult(imag_sample[t],sin_angle));
-        outreal = add(real_part,outreal);
+        sumreal = add(real_part,sumreal);
         const DTYPE imag_part = add(mult(real_sample[t],sin_angle),mult(imag_sample[t],cos_angle));
-        outimag = add(imag_part,outimag);
+        sumimag = add(imag_part,sumimag);
     }
+    outreal = sumreal;
+    outimag = sumimag;
 }
 
 void dft(DTYPE real_sample[SIZE], DTYPE imag_sample[SIZE], DTYPE outreal[SIZE], DTYPE outimag[SIZE])
